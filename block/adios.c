ee7798346adf24205f24d61b937a6aa1813b8d34
Pzqqt <821026875@qq.com>
2026-04-26 17:29:44 +0800

    block: adios: Ignore flush/fua requests when handling the completion of a request
    This is also the main reason why the `rd` variable in the `adios_completed_request` function is sometimes a null pointer.
    
    The upstream handles this by setting and getting the newly added `RQF_USE_SCHED` flag, but we don't have these commits, so we can only handle it internally in the IO scheduler.
    
    Reference upstream patch:
    https://github.com/torvalds/linux/commit/dd6216bb16e83e349d5d987227328031b0b0d30d
    
    Signed-off-by: Pzqqt <821026875@qq.com>

--- block/adios.c
+++ block/adios.c
@@ -1392,7 +1392,7 @@ static void adios_completed_request(struct request *rq, u64 now) {
 	struct adios_rq_data *rd = get_rq_data(rq);
 	union adios_in_flight_rqs ifr = { .scalar = 0 };
 
-	if (!rd)
+	if (op_is_flush(rq->cmd_flags) || !rd)
 		return;
 
 	if (rd->managed) {
